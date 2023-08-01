c3c4a77caaa27958c76e1fcf5cbcb00149bb452b
thiagorodcarv <33536075+thiagorodcarv@users.noreply.github.com>
2023-08-01 11:50:03 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,18 +9,16 @@ static coap_context_t *coap_ctx = NULL;
 static void handle_get_contador(coap_context_t *ctx, struct coap_resource_t *resource,
                                 const coap_endpoint_t *local_interface, coap_address_t *peer,
                                 coap_pdu_t *request, coap_pdu_t *response) {
-    ESP_LOGI(TAG, "Received a GET request for contador");
+    ESP_LOGI(TAG, "Received a GET request for contador"); // Registra no log que uma requisição GET foi recebida
+
     char payload[16];
-    snprintf(payload, sizeof(payload), "%d", contador);
+    snprintf(payload, sizeof(payload), "%d", contador); // Converte o valor do contador para uma string
 
-    // Definir o código de resposta (2.05 Content)
-    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT); // Define o código de resposta como 2.05 Content
 
-    // Definir o payload da resposta
-    coap_add_data(response, strlen(payload), (const uint8_t *)payload);
+    coap_add_data(response, strlen(payload), (const uint8_t *)payload); // Define o payload da resposta com o valor do contador
 
-    // Enviar a resposta
-    coap_send(ctx, response);
+    coap_send(ctx, response); // Envia a resposta para o cliente que fez a requisição
 }
 
 // Função para notificar observadores com dados personalizados
@@ -30,26 +28,23 @@ static void notify_observers() {
         return;
     }
 
-    // Criar uma resposta de notificação personalizada
+    // Cria uma resposta de notificação personalizada
     coap_pdu_t *notification = coap_pdu_init(0, 0, 0, 200);
     if (!notification) {
         ESP_LOGE(TAG, "Failed to create CoAP notification PDU");
         return;
     }
 
-    // Definir o código de resposta (2.05 Content)
-    coap_pdu_set_code(notification, COAP_RESPONSE_CODE_CONTENT);
+    coap_pdu_set_code(notification, COAP_RESPONSE_CODE_CONTENT); // Define o código de resposta como 2.05 Content
 
-    // Definir o payload da notificação
     const char *data = "aumento identificado";
-    coap_add_data(notification, strlen(data), (const uint8_t *)data);
+    coap_add_data(notification, strlen(data), (const uint8_t *)data); // Define o payload da notificação com uma mensagem
 
-    // Notificar observadores registrados para o recurso "contador"
-    coap_resource_notify_observers(notification, "/contador");
+    coap_resource_notify_observers(notification, "/contador"); // Notifica observadores registrados para o recurso "contador"
 
-    // Liberar recursos
-    coap_delete_pdu(notification);
+    coap_delete_pdu(notification); // Libera os recursos da notificação
 }
+
 
 void coap_server_task(void *pvParameters) {
     coap_address_t addr;
@@ -60,31 +55,31 @@ void coap_server_task(void *pvParameters) {
     addr.addr.sin.sin_port = htons(COAP_DEFAULT_PORT);
     addr.addr.sin.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    coap_ctx = coap_new_context(&addr);
+    coap_ctx = coap_new_context(&addr); // Cria o contexto do servidor CoAP
     if (!coap_ctx) {
         ESP_LOGE(TAG, "Failed to create CoAP context");
         vTaskDelete(NULL);
     }
 
-    endpoint = coap_new_endpoint(coap_ctx, &addr, COAP_PROTO_UDP);
+    endpoint = coap_new_endpoint(coap_ctx, &addr, COAP_PROTO_UDP); // Cria o endpoint UDP para comunicação
     if (!endpoint) {
         ESP_LOGE(TAG, "Failed to create CoAP endpoint");
         coap_free_context(coap_ctx);
         vTaskDelete(NULL);
     }
 
-    // Registro do recurso "contador" no servidor
-    coap_resource_t *contador_resource = coap_resource_init((unsigned char *)"contador", 0);
-    coap_register_handler(contador_resource, COAP_REQUEST_GET, handle_get_contador);
-    coap_add_resource(coap_ctx, contador_resource);
+    coap_resource_t *contador_resource = coap_resource_init((unsigned char *)"contador", 0); // Cria o recurso "contador"
+    coap_register_handler(contador_resource, COAP_REQUEST_GET, handle_get_contador); // Registra o tratamento para a operação GET do recurso
+    coap_add_resource(coap_ctx, contador_resource); // Adiciona o recurso ao contexto do servidor
 
     while (1) {
-        contador++;
+        contador++; // Incrementa o contador
         ESP_LOGI(TAG, "Incrementing contador: %d", contador);
-        notify_observers();
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        notify_observers(); // Notifica os observadores sobre o aumento do contador
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Aguarda 5 segundos antes de repetir o processo
     }
 
+    // Libera os recursos utilizados pelo servidor CoAP antes de encerrar a tarefa
     coap_delete_resource(coap_ctx, contador_resource);
     coap_free_endpoint(endpoint);
     coap_free_context(coap_ctx);
@@ -93,9 +88,10 @@ void coap_server_task(void *pvParameters) {
     vTaskDelete(NULL);
 }
 
+
 void app_main() {
     // Inicialização do ESP-IDF e outras configurações necessárias
 
-    // Crie uma tarefa para o servidor CoAP
-    xTaskCreate(&coap_server_task, "coap_server_task", 4096, NULL, 5, NULL);
+    xTaskCreate(&coap_server_task, "coap_server_task", 4096, NULL, 5, NULL); // Cria uma tarefa para o servidor CoAP
 }
+
